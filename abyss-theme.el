c21c0c766a0fb9ed11a7b32d600eab5d66247824
Matt Russell <mattr@netsight.co.uk>
2015-10-04 21:06:38 +0100

    Small case-correction in commentary.

--- abyss-theme.el
+++ abyss-theme.el
@@ -9,7 +9,7 @@
 
 ;;; Commentary:
 
-;; A Dark Theme with contrasting colours for Emacs24 based on the
+;; A dark theme with contrasting colours for Emacs24 based on the
 ;; ``Lush`` theme by Andre Richter, using the same colours palette
 ;; as the the built-in `dichromacy' theme; intended to be suitable
 ;; for red/green colour blind users.
